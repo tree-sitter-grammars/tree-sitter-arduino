c473c09f23b13d9404ec5208fad3eddea89d70f8
ObserverOfTime <chronobserver@disroot.org>
2022-11-16 17:25:39 +0200

    Remove Scanner struct

--- src/scanner.cc
+++ src/scanner.cc
@@ -2,8 +2,6 @@
 #include <string>
 #include <cwctype>
 
-namespace {
-
 using std::wstring;
 using std::iswspace;
 
@@ -11,109 +9,96 @@ enum TokenType {
   RAW_STRING_LITERAL,
 };
 
-struct Scanner {
-  bool scan(TSLexer *lexer, const bool *valid_symbols) {
-    while (iswspace(lexer->lookahead)) {
-      lexer->advance(lexer, true);
+extern "C" {
+
+void *tree_sitter_arduino_external_scanner_create() {
+  return nullptr;
+}
+
+bool tree_sitter_arduino_external_scanner_scan(void *payload, TSLexer *lexer,
+                                               const bool *valid_symbols) {
+  while (iswspace(lexer->lookahead)) {
+    lexer->advance(lexer, true);
+  }
+
+  lexer->result_symbol = RAW_STRING_LITERAL;
+
+  // Raw string literals can start with: R, LR, uR, UR, u8R
+  // Consume 'R'
+  if (lexer->lookahead == 'L' || lexer->lookahead == 'U') {
+    lexer->advance(lexer, false);
+    if (lexer->lookahead != 'R') {
+      return false;
     }
-
-    lexer->result_symbol = RAW_STRING_LITERAL;
-
-    // Raw string literals can start with: R, LR, uR, UR, u8R
-    // Consume 'R'
-    if (lexer->lookahead == 'L' || lexer->lookahead == 'U') {
+  } else if (lexer->lookahead == 'u') {
+    lexer->advance(lexer, false);
+    if (lexer->lookahead == '8') {
       lexer->advance(lexer, false);
       if (lexer->lookahead != 'R') {
-        return false;
-      }
-    } else if (lexer->lookahead == 'u') {
-      lexer->advance(lexer, false);
-      if (lexer->lookahead == '8') {
-        lexer->advance(lexer, false);
-        if (lexer->lookahead != 'R') {
-          return false;
-        }
-      } else if (lexer->lookahead != 'R') {
         return false;
       }
     } else if (lexer->lookahead != 'R') {
       return false;
     }
-    lexer->advance(lexer, false);
+  } else if (lexer->lookahead != 'R') {
+    return false;
+  }
+  lexer->advance(lexer, false);
 
-    // Consume '"'
-    if (lexer->lookahead != '"') return false;
-    lexer->advance(lexer, false);
+  // Consume '"'
+  if (lexer->lookahead != '"') return false;
+  lexer->advance(lexer, false);
 
-    // Consume '(', delimiter
-    wstring delimiter;
-    for (;;) {
-      if (lexer->lookahead == 0 || lexer->lookahead == '\\' || iswspace(lexer->lookahead)) {
-        return false;
-      }
-      if (lexer->lookahead == '(') {
-        lexer->advance(lexer, false);
-        break;
-      }
-      delimiter += lexer->lookahead;
-      lexer->advance(lexer, false);
+  // Consume '(', delimiter
+  wstring delimiter;
+  for (;;) {
+    if (lexer->lookahead == 0 || lexer->lookahead == '\\' || iswspace(lexer->lookahead)) {
+      return false;
     }
+    if (lexer->lookahead == '(') {
+      lexer->advance(lexer, false);
+      break;
+    }
+    delimiter += lexer->lookahead;
+    lexer->advance(lexer, false);
+  }
 
-    // Consume content, delimiter, ')', '"'
-    int delimiter_index = -1;
-    for (;;) {
-      if (lexer->lookahead == 0) return false;
+  // Consume content, delimiter, ')', '"'
+  int delimiter_index = -1;
+  for (;;) {
+    if (lexer->lookahead == 0) return false;
 
-      if (delimiter_index >= 0) {
-        if (static_cast<unsigned>(delimiter_index) == delimiter.size()) {
-          if (lexer->lookahead == '"') {
-            lexer->advance(lexer, false);
-            return true;
-          } else {
-            delimiter_index = -1;
-          }
+    if (delimiter_index >= 0) {
+      if (static_cast<unsigned>(delimiter_index) == delimiter.size()) {
+        if (lexer->lookahead == '"') {
+          lexer->advance(lexer, false);
+          return true;
         } else {
-          if (lexer->lookahead == delimiter[delimiter_index]) {
-            delimiter_index++;
-          } else {
-            delimiter_index = -1;
-          }
+          delimiter_index = -1;
+        }
+      } else {
+        if (lexer->lookahead == delimiter[delimiter_index]) {
+          delimiter_index++;
+        } else {
+          delimiter_index = -1;
         }
       }
-
-      if (delimiter_index == -1 && lexer->lookahead == ')') {
-        delimiter_index = 0;
-      }
-
-      lexer->advance(lexer, false);
     }
+
+    if (delimiter_index == -1 && lexer->lookahead == ')') {
+      delimiter_index = 0;
+    }
+
+    lexer->advance(lexer, false);
   }
-};
-
-}
-
-extern "C" {
-
-void *tree_sitter_arduino_external_scanner_create() {
-  return new Scanner();
-}
-
-bool tree_sitter_arduino_external_scanner_scan(void *payload, TSLexer *lexer,
-                                               const bool *valid_symbols) {
-  Scanner *scanner = static_cast<Scanner *>(payload);
-  return scanner->scan(lexer, valid_symbols);
 }
 
 unsigned tree_sitter_arduino_external_scanner_serialize(void *payload, char *buffer) {
   return 0;
 }
 
-void tree_sitter_arduino_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
-}
+void tree_sitter_arduino_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {}
 
-void tree_sitter_arduino_external_scanner_destroy(void *payload) {
-  Scanner *scanner = static_cast<Scanner *>(payload);
-  delete scanner;
-}
+void tree_sitter_arduino_external_scanner_destroy(void *payload) {}
 
 }
