0235b03632341a697395bf20feaeff5f4f6dc5d6
ObserverOfTime <chronobserver@disroot.org>
2024-02-11 16:53:42 +0200

    chore: add sample code to rust binding

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -4,10 +4,22 @@
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
-//! let code = "";
+//! let code = r#"
+//!     void setup() {
+//!       pinMode(LED_BUILTIN, OUTPUT);
+//!     }
+//!
+//!     void loop() {
+//!       digitalWrite(LED_BUILTIN, HIGH);
+//!       delay(1000);
+//!       digitalWrite(LED_BUILTIN, LOW);
+//!       delay(1000);
+//!     }
+//! "#;
 //! let mut parser = tree_sitter::Parser::new();
 //! parser.set_language(tree_sitter_arduino::language()).expect("Error loading arduino grammar");
 //! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
@@ -33,12 +45,6 @@ pub fn language() -> Language {
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
-// Uncomment these to include any queries that this grammar contains
-
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
